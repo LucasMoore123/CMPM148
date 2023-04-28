f740df91fd36ea563186d95d68401561bac8a723
LucasMoore123 <66981809+LucasMoore123@users.noreply.github.com>
2023-04-28 13:43:32 -0700

    Add files via upload

--- assignment3/seller.js
+++ assignment3/seller.js
@@ -1,12 +1,12 @@
 function generate_seller(){
   result = "<ol>"
   seller = {
-	"origin":["Your name is #firstname# #lastname#. You are a(n) #ownerDesc# merchant. You are #ownerPricing# in regards to your pricing."],
-	"firstname":["Theren", "Gwendolyn", "Zephyr", "Kaelen", "Morgath", "Drogath", "Galadriel", "Draven", "Mordekai", "Xander", "Yara", "Niamh", "Rhiannon", "Emeric", "Kethryllia", "Thia", "Asher", "Elden", "Valerius", "Sariel"],
-	"lastname":["Blackwood", "Moonshadow", "Bloodfist", "Silverscale", "Ironheart", "Goldflame", "Stormweaver", "Ravenwing", "Frostblade", "Stonebreaker", "Dragonborn", "Starweaver", "Swordhand", "Brightsoul", "Windrider", "Bloodstone", "Shadowhunter", "Thornbush", "Stormcaller", "Flintlock"],
-	"ownerDesc":["wizened", "grizzled", "eccentric", "mysterious", "shrewd", "boisterous", "stoic", "flamboyant", "crotchety", "gregarious", "fastidious", "pompous", "witty", "quirky", "eloquent", "obstinate", "persuasive", "jovial", "sly", "kind-hearted"],
-	"ownerPricing":["inflexible", "steadfast", "resolute", "unyielding", "stubborn", "firm", "reasonable", "open-minded", "flexible", "pragmatic", "amenable", "adaptive", "accommodating", "capricious", "fickle", "changeable", "unpredictable", "impulsive", "volatile", "mercurial"]
-} 
+    "origin":["Your name is #firstname# #lastname#. You are a(n) #ownerDesc# merchant. You are #ownerPricing# in regards to your pricing."],
+    "firstname":["Theren", "Gwendolyn", "Zephyr", "Kaelen", "Morgath", "Drogath", "Galadriel", "Draven", "Mordekai", "Xander", "Yara", "Niamh", "Rhiannon", "Emeric", "Kethryllia", "Thia", "Asher", "Elden", "Valerius", "Sariel"],
+    "lastname":["Blackwood", "Moonshadow", "Bloodfist", "Silverscale", "Ironheart", "Goldflame", "Stormweaver", "Ravenwing", "Frostblade", "Stonebreaker", "Dragonborn", "Starweaver", "Swordhand", "Brightsoul", "Windrider", "Bloodstone", "Shadowhunter", "Thornbush", "Stormcaller", "Flintlock"],
+    "ownerDesc":["wizened", "grizzled", "eccentric", "mysterious", "shrewd", "boisterous", "stoic", "flamboyant", "crotchety", "gregarious", "fastidious", "pompous", "witty", "quirky", "eloquent", "obstinate", "persuasive", "jovial", "sly", "kind-hearted"],
+    "ownerPricing":["inflexible", "steadfast", "resolute", "unyielding", "stubborn", "firm", "reasonable", "open-minded", "flexible", "pragmatic", "amenable", "adaptive", "accommodating", "capricious", "fickle", "changeable", "unpredictable", "impulsive", "volatile", "mercurial"]
+  }
   for (let i = 0; i < 6; i++){
     result += "<li>" + grammars.GenerationSimple(seller) + "</li>";
   }
